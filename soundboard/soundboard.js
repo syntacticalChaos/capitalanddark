47dc4bd4f33b6c27ce805404fb1a81956725a454
la-uni <65335086+la-uni@users.noreply.github.com>
2020-09-02 21:01:56 +1000

    Add files via upload

---
+++ soundboard/soundboard.js
@@ -0,0 +1,226 @@
+//QWERTYUIOP - line 1
+//immanence becomes imminence the ash of annihilation lands softly on
+//Q -- immanence
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 81 ) {
+        document.getElementById('immanence').play()
+        document.getElementById("words").innerHTML = "immanence"
+    }
+})
+
+//W -- becomes
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 87) {
+        document.getElementById('becomes').play()
+        document.getElementById("words").innerHTML = "becomes"
+    }
+})
+
+//E -- imminence
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 69) {
+        document.getElementById('imminence').play()
+        document.getElementById("words").innerHTML = "imminence"
+    }
+})
+
+//R -- the
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 82) {
+        document.getElementById('the1').play()
+        document.getElementById("words").innerHTML = "the"
+    }
+})
+
+//T
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 84) {
+        document.getElementById('ash').play()
+        document.getElementById("words").innerHTML = "ash"
+    }
+})
+
+//Y
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 89) {
+        document.getElementById('of').play()
+        document.getElementById("words").innerHTML = "of"
+    }
+})
+
+//U
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 85) {
+        document.getElementById('annihilation').play()
+        document.getElementById("words").innerHTML = "annihilation"
+    }
+})
+
+//I
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 73) {
+        document.getElementById('lands').play()
+        document.getElementById("words").innerHTML = "lands"
+    }
+})
+
+//O
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 79) {
+        document.getElementById('softly').play()
+        // document.getElementById("body").style.backgroundColor = "Yellow"
+        document.getElementById("words").innerHTML = "softly"
+    }
+})
+
+//P
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 80) {
+        document.getElementById('on').play()
+        // document.getElementById("body").style.backgroundColor = "MediumOrchid"
+        document.getElementById("words").innerHTML = "on"
+    }
+})
+
+
+//ASDFGHJKL - line 2
+//lungs of those watching the sun darkens in its
+
+//A
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+        document.getElementById('lungs').play()
+        document.getElementById("words").innerHTML = "lungs"
+    }
+})
+
+//S
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+        document.getElementById('of').play()
+        document.getElementById("words").innerHTML = "of"
+    }
+})
+
+//D
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+        document.getElementById('those').play()
+        document.getElementById("words").innerHTML = "those"
+    }
+})
+
+//F
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+        document.getElementById('watching').play()
+        document.getElementById("words").innerHTML = "watching"
+    }
+})
+
+//G
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+        document.getElementById('the2').play()
+        document.getElementById("words").innerHTML = "the"
+    }
+})
+
+//H
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+        document.getElementById('sun').play()
+        document.getElementById("words").innerHTML = "sun"
+    }
+})
+
+
+//J
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+        document.getElementById('darkens').play()
+        // document.getElementById("body").style.backgroundColor = "Crimson"
+        document.getElementById("words").innerHTML = "darkens"
+    }
+})
+
+//K
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+        document.getElementById('in').play()
+        document.getElementById("words").innerHTML = "in"
+    }
+})
+
+//L
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+        document.getElementById('its').play()
+        document.getElementById("words").innerHTML = "its"
+    }
+})
+
+//ZXCVBNM - line 3
+//brightness I am already word without end
+//Z
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 90) {
+        document.getElementById('brightness').play()
+        document.getElementById("words").innerHTML = "brightness"
+
+    }
+})
+
+//X
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 88) {
+        document.getElementById('I').play()
+        document.getElementById("words").innerHTML = "I"
+    }
+})
+
+
+//C
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 67) {
+        document.getElementById('am').play()
+        // document.getElementById("body").style.backgroundColor = "SteelBlue"
+        document.getElementById("words").innerHTML = "am"
+
+    }
+})
+
+//V
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 86) {
+        document.getElementById('already').play()
+        document.getElementById("words").innerHTML = "already"
+
+    }
+})
+
+//B
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 66) {
+        document.getElementById('word').play()
+        document.getElementById("words").innerHTML = "word"
+    }
+})
+
+//N
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 78) {
+        document.getElementById('without').play()
+        // document.getElementById("body").style.backgroundColor = "Thistle"
+        document.getElementById("words").innerHTML = "without"
+
+    }
+})
+
+//M
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 77) {
+        document.getElementById('end').play()
+        document.getElementById("words").innerHTML = "end"
+    }
+})
