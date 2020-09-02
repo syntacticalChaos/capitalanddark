4a7d7ded47b05804a291aebc4890a8972d5403aa
la-uni <65335086+la-uni@users.noreply.github.com>
2020-09-02 22:32:48 +1000

    Update soundboard.js

--- soundboard/soundboard.js
+++ soundboard/soundboard.js
@@ -1,226 +1,139 @@
+document.addEventListener ('keydown', function(e) {
 //QWERTYUIOP - line 1
 //immanence becomes imminence the ash of annihilation lands softly on
 //Q -- immanence
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 81 ) {
-        document.getElementById('immanence').play()
-        document.getElementById("words").innerHTML = "immanence"
-    }
-})
+if (e.keyCode == 81) {
+  document.getElementById('immanence').play()
+  document.getElementById("words").innerHTML = "immanence"
+}
 
 //W -- becomes
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 87) {
-        document.getElementById('becomes').play()
-        document.getElementById("words").innerHTML = "becomes"
-    }
-})
-
+  if (e.keyCode == 87) {
+    document.getElementById('becomes').play()
+    document.getElementById("words").innerHTML = "becomes"
+  }
 //E -- imminence
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 69) {
-        document.getElementById('imminence').play()
-        document.getElementById("words").innerHTML = "imminence"
-    }
-})
-
+  if (e.keyCode == 69) {
+      document.getElementById('imminence').play()
+      document.getElementById("words").innerHTML = "imminence"
+  }
 //R -- the
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 82) {
-        document.getElementById('the1').play()
-        document.getElementById("words").innerHTML = "the"
-    }
-})
-
-//T
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 84) {
-        document.getElementById('ash').play()
-        document.getElementById("words").innerHTML = "ash"
-    }
-})
-
-//Y
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 89) {
-        document.getElementById('of').play()
-        document.getElementById("words").innerHTML = "of"
-    }
-})
-
-//U
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 85) {
-        document.getElementById('annihilation').play()
-        document.getElementById("words").innerHTML = "annihilation"
-    }
-})
-
-//I
-document.addEventListener ('keydown', function(e) {
-    if (e.keyCode == 73) {
-        document.getElementById('lands').play()
-        document.getElementById("words").innerHTML = "lands"
-    }
-})
-
-//O
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 79) {
-        document.getElementById('softly').play()
-        // document.getElementById("body").style.backgroundColor = "Yellow"
-        document.getElementById("words").innerHTML = "softly"
-    }
-})
-
-//P
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 80) {
-        document.getElementById('on').play()
-        // document.getElementById("body").style.backgroundColor = "MediumOrchid"
-        document.getElementById("words").innerHTML = "on"
-    }
-})
-
-
+  if (e.keyCode == 82) {
+    document.getElementById('the1').play()
+    document.getElementById("words").innerHTML = "the"
+  }
+//T -- ash
+  if (e.keyCode == 84) {
+    document.getElementById('ash').play()
+    document.getElementById("words").innerHTML = "ash"
+  }
+//Y -- of
+  if (e.keyCode == 89) {
+    document.getElementById('of').play()
+    document.getElementById("words").innerHTML = "of"
+  }
+//U -- annihilation
+  if (e.keyCode == 85) {
+    document.getElementById('annihilation').play()
+    document.getElementById("words").innerHTML = "annihilation"
+  }
+//I -- lands
+  if (e.keyCode == 73) {
+    document.getElementById('lands').play()
+    document.getElementById("words").innerHTML = "lands"
+  }
+//O -- softly
+ if (e.keyCode == 79) {
+   document.getElementById('softly').play()
+   document.getElementById("words").innerHTML = "softly"
+ }
+//P -- on
+  if (e.keyCode == 80) {
+    document.getElementById('on').play()
+    document.getElementById("words").innerHTML = "on"
+  }
 //ASDFGHJKL - line 2
 //lungs of those watching the sun darkens in its
-
-//A
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 65) {
-        document.getElementById('lungs').play()
-        document.getElementById("words").innerHTML = "lungs"
+//A -- lungs
+  if (e.keyCode == 65) {
+    document.getElementById('lungs').play()
+    document.getElementById("words").innerHTML = "lungs"
+  }
+//S -- of
+  if (e.keyCode == 83) {
+    document.getElementById('of').play()
+    document.getElementById("words").innerHTML = "of"
+  }
+//D -- those
+  if (e.keyCode == 68) {
+    document.getElementById('those').play()
+    document.getElementById("words").innerHTML = "those"
+  }
+//F-- watching
+  if (e.keyCode == 70) {
+    document.getElementById('watching').play()
+    document.getElementById("words").innerHTML = "watching"
     }
-})
-
-//S
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 83) {
-        document.getElementById('of').play()
-        document.getElementById("words").innerHTML = "of"
+//G -- the
+  if (e.keyCode == 71) {
+    document.getElementById('the2').play()
+    document.getElementById("words").innerHTML = "the"
+  }
+//H -- sun
+  if (e.keyCode == 72) {
+    document.getElementById('sun').play()
+    document.getElementById("words").innerHTML = "sun"
+  }
+//J -- darkens
+  if (e.keyCode == 74) {
+    document.getElementById('darkens').play()
+    document.getElementById("words").innerHTML = "darkens"
     }
-})
-
-//D
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 68) {
-        document.getElementById('those').play()
-        document.getElementById("words").innerHTML = "those"
+//K -- in
+  if (e.keyCode == 75) {
+    document.getElementById('in').play()
+    document.getElementById("words").innerHTML = "in"
     }
-})
-
-//F
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 70) {
-        document.getElementById('watching').play()
-        document.getElementById("words").innerHTML = "watching"
+//L -- its
+  if (e.keyCode == 76) {
+    document.getElementById('its').play()
+    document.getElementById("words").innerHTML = "its"
     }
-})
-
-//G
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 71) {
-        document.getElementById('the2').play()
-        document.getElementById("words").innerHTML = "the"
-    }
-})
-
-//H
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 72) {
-        document.getElementById('sun').play()
-        document.getElementById("words").innerHTML = "sun"
-    }
-})
-
-
-//J
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 74) {
-        document.getElementById('darkens').play()
-        // document.getElementById("body").style.backgroundColor = "Crimson"
-        document.getElementById("words").innerHTML = "darkens"
-    }
-})
-
-//K
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 75) {
-        document.getElementById('in').play()
-        document.getElementById("words").innerHTML = "in"
-    }
-})
-
-//L
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 76) {
-        document.getElementById('its').play()
-        document.getElementById("words").innerHTML = "its"
-    }
-})
-
 //ZXCVBNM - line 3
 //brightness I am already word without end
-//Z
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 90) {
-        document.getElementById('brightness').play()
-        document.getElementById("words").innerHTML = "brightness"
-
+//Z -- brightness
+  if (e.keyCode == 90) {
+    document.getElementById('brightness').play()
+    document.getElementById("words").innerHTML = "brightness"
     }
-})
-
-//X
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 88) {
-        document.getElementById('I').play()
-        document.getElementById("words").innerHTML = "I"
+//X -- I
+  if (e.keyCode == 88) {
+    document.getElementById('I').play()
+    document.getElementById("words").innerHTML = "I"
     }
-})
-
-
-//C
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 67) {
-        document.getElementById('am').play()
-        // document.getElementById("body").style.backgroundColor = "SteelBlue"
-        document.getElementById("words").innerHTML = "am"
-
+//C -- am
+  if (e.keyCode == 67) {
+    document.getElementById('am').play()
+    document.getElementById("words").innerHTML = "am"
     }
-})
-
-//V
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 86) {
-        document.getElementById('already').play()
-        document.getElementById("words").innerHTML = "already"
-
+//V -- already
+  if (e.keyCode == 86) {
+    document.getElementById('already').play()
+    document.getElementById("words").innerHTML = "already"
     }
-})
-
-//B
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 66) {
-        document.getElementById('word').play()
-        document.getElementById("words").innerHTML = "word"
+//B -- word
+  if (e.keyCode == 66) {
+    document.getElementById('word').play()
+    document.getElementById("words").innerHTML = "word"
     }
-})
-
-//N
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 78) {
-        document.getElementById('without').play()
-        // document.getElementById("body").style.backgroundColor = "Thistle"
-        document.getElementById("words").innerHTML = "without"
-
+//N -- without
+  if (e.keyCode == 78) {
+    document.getElementById('without').play()
+    document.getElementById("words").innerHTML = "without"
     }
-})
-
-//M
-document.addEventListener ('keydown', function(e) {
-    if (e.keyCode == 77) {
-        document.getElementById('end').play()
-        document.getElementById("words").innerHTML = "end"
+//M -- end
+  if (e.keyCode == 77) {
+    document.getElementById('end').play()
+    document.getElementById("words").innerHTML = "end"
     }
 })
